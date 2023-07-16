544308fa8acdcf553869b787ddf8b6feec8807d0
IsiAddy <136353617+IsiAddy@users.noreply.github.com>
2023-07-16 17:50:50 +0000

    Add files via upload

---
+++ Q on Magic number.py
@@ -0,0 +1,17 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+magic_number= 50
+
+number =  int(input("Enter the number ..  "))
+if number < 50:
+  print("That number is too low")
+elif number > 50:
+   print("That number is too high")
+else:
+    print("Hooray! You got it correct")
